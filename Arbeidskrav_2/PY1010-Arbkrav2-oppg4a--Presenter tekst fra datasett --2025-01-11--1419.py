c65dfb020f66feb3ac1e650b5c80268d9b86c2fe
Frode Meyer (pri) <97193643+FrmeFrme@users.noreply.github.com>
2025-01-14 20:48:59 +0100

    Add files via upload

---
+++ Arbeidskrav_2/PY1010-Arbkrav2-oppg4a--Presenter tekst fra datasett --2025-01-11--1419.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 11 14:17:23 2025
+Program som presenterer tekst ut fra et gitt datasett
+@author: frode meyer
+"""
+
+# Datasett
+data = { "Norge": ["Oslo", 0.634], "England": ["London", 8.982], "Frankrike": ["Paris", 2.161], "Italia": ["Roma", 2.873] }
+        
+# Innhent hvilket land som skal presenteres
+angi_land = str(input("Hvilket land ønsker du informasjon om? "))
+
+# Utfør oppslag i datasettet, og presenter informasjon om landet. Gi info om landet ikke finnes i datasettet
+if angi_land in data:
+    info = data[angi_land]
+    print(f"Land: {angi_land}, Hovedstad: {info[0]}, Befolkning: {info[1]}  [milloner]")
+else:
+    print("Informasjon om landet finnes ikke.")
