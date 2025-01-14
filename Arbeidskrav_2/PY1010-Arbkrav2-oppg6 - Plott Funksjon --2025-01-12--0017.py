40c0b81bee9aa5ea874f25e84af3924ffac7df9d
Frode Meyer (pri) <97193643+FrmeFrme@users.noreply.github.com>
2025-01-14 21:27:21 +0100

    Update PY1010-Arbkrav2-oppg6 - Plott Funksjon --2025-01-12--0017.py

--- Arbeidskrav_2/PY1010-Arbkrav2-oppg6 - Plott Funksjon --2025-01-12--0017.py
+++ Arbeidskrav_2/PY1010-Arbkrav2-oppg6 - Plott Funksjon --2025-01-12--0017.py
@@ -17,12 +17,23 @@ xa = np.linspace(-10,10, 200)
 
 # Plott funksjonen med oppgitt nedre og øvre grense, med 200 punktsoppløsning
 # med beskrivelse og tittel
+
+# Slett eventuelt tidligere grafer
 plt.close('all')
-plt.plot(xa, f(xa))
-plt.xlabel('x')
-plt.ylabel('y')
-plt.title('Funksjonen f(x) = -x² - 5')
-plt.legend(labels=('f(x) = -x² - 5'))
+
+# Grafen som skal plottes, med etikett beskrivelse
+plt.plot(xa, f(xa), label='f(x) = -x² - 5')  # Legg til etikett for funksjonen
+
+# Legg til en vertikal linje ved x=0
+plt.axvline(x=0, color='black', linestyle='--')  # Vertikal linje ved x-aksens verdi 0
+
+# Legg til tekst til x og y aksen
+plt.xlabel('x-verdi')
+plt.ylabel('y-verdi')
+
+#L Legg til tittel til grafen
+plt.title('Graf til funksjonen f(x) = -x² - 5')
+plt.legend()
 
 # Vis plottet
 plt.show()
