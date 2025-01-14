a39549436d696567631f284e83bbaeec075a7fd3
Frode Meyer (pri) <97193643+FrmeFrme@users.noreply.github.com>
2025-01-14 21:05:47 +0100

    Update PY1010-Arbkrav2-oppg4a--Presenter tekst fra datasett --2025-01-11--1419.py

--- Arbeidskrav_2/PY1010-Arbkrav2-oppg4a--Presenter tekst fra datasett --2025-01-11--1419.py
+++ Arbeidskrav_2/PY1010-Arbkrav2-oppg4a--Presenter tekst fra datasett --2025-01-11--1419.py
@@ -8,8 +8,8 @@ Program som presenterer tekst ut fra et gitt datasett
 # Datasett
 data = { "Norge": ["Oslo", 0.634], "England": ["London", 8.982], "Frankrike": ["Paris", 2.161], "Italia": ["Roma", 2.873] }
         
-# Innhent hvilket land som skal presenteres
-angi_land = str(input("Hvilket land ønsker du informasjon om? "))
+# Innhent hvilket land som skal presenteres. Første bokstav konverteres alltid til stor bokstav.
+angi_land = str(input("Hvilket land ønsker du informasjon om? ")).capitalize()
 
 # Utfør oppslag i datasettet, og presenter informasjon om landet. Gi info om landet ikke finnes i datasettet
 if angi_land in data:
